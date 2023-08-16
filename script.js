28a94cf13d527dd535b280c8d45eb908fc379857
bokkle <mitch.r_@hotmail.com>
2023-08-15 22:11:55 -0400

    update

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".minute-hand");
 const hourHand = document.querySelector(".hour-hand");
 
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
+
 const setDate = () => {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -9,14 +13,12 @@ const setDate = () => {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes + seconds / 60) / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees =
-    ((hours + minutes / 60 + seconds / 3600) / 12) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 };
 
 setInterval(setDate, 1000);
-setDate(); // Initial call to set initial positions
